012a8397b0d924051d175a9fa236f4cc72d0c93f
STANLEY\YiuPang <stanley.yuen@itpunch.com>
2019-01-02 22:40:03 +0800

    update Cloud frontmation template
    update Cloud frontmation template

--- backend/ipdetection_test.go
+++ backend/ipdetection_test.go
@@ -7,7 +7,7 @@ import (
    "net/http/httptest"
    "testing"
    "github.com/gin-gonic/gin"
-   	"github.com/stretchr/testify/assert"
+   "github.com/stretchr/testify/assert"
 )
 
 func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
