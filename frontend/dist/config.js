accdfd9bd85fe0b57e291439c43375f8c6945aa0
STANLEY\YiuPang <stanley.yuen@itpunch.com>
2019-01-02 23:34:25 +0800

    Update config.js
    Update config.js

--- frontend/dist/config.js
+++ frontend/dist/config.js
@@ -3,14 +3,14 @@ var awsConfig = {
 		endpoints: [
             {
                 name: "ips",
-                endpoint: "https://6ra59j0si4.execute-api.ap-southeast-1.amazonaws.com/v1",
-                region: "ap-southeast-1"
+                endpoint: "{ApiBaseUrl}",
+                region: "{Region}"
             },
         ]
 	},
 	Auth:{
-		identityPoolId: 'ap-southeast-1:517634f3-a741-44e4-ab36-ea848d6964b8', 
-        region: 'ap-southeast-1'
+		identityPoolId: "{CognitoId}", 
+        region: "{Region}"
 	}
 
 }
